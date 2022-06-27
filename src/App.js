adee5a543e7461f6226c098f78f9a51dbe2fcfc8
matiasc18 <matiascarulli@gmail.com>
2022-06-26 21:35:13 -0400

    Renamed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import SignupPage from './components/SignupPage';
-import Games from './components/Games';
+import SignupPage from './pages/SignupPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Games />} />
+          <Route path='/' element={<HomePage />} />
           <Route path='/signup' element={<SignupPage />} />
         </Routes>
       </BrowserRouter>
