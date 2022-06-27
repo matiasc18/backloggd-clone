13ed29f5c098e5ca2295a1146d9389a775bcdbf5
matiasc18 <matiascarulli@gmail.com>
2022-06-26 21:36:17 -0400

    Fixed class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import '../styles/header.css';
 const Header = () => {
   return (
     <header>
-      <Link to='/' id="header-title">Frontloggd</Link>
-      <nav class="nav-links">
+      <Link to='/' id="header-title"><code>{`<frontloggd>`}</code></Link>
+      <nav className="nav-links">
         <Link to='/games'>Games</Link>
-        <Link to='/'>Sign Up</Link>
+        <Link to='/signup'>Sign Up</Link>
         <Link to='/'>Log In</Link>
       </nav>
     </header>
