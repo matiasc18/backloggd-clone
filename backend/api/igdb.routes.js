6a60ea3199212b427d769de3340851ff009ec3c8
matiasc18 <matiascarulli@gmail.com>
2022-06-26 21:34:17 -0400

    igdb/games request

--- backend/api/igdb.routes.js
+++ backend/api/igdb.routes.js
@@ -2,28 +2,29 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-// GET: Get all games
+// * GET: Get all games
 router.route('/').post(async (req, res) => {
-  const body = `fields ${req.body.fields}; ${req.body.content}`;
 
+  console.log(req.body);
+
+  // Default config for reguesting to IGDB
   const config = {
     method: 'post',
     url: '/games',
     baseURL: process.env.IGDB_URI,
     headers: {
-      'Content-Type': 'text/plain',
+      'Accept': 'application/json',
       'Client-ID': process.env.CLIENT_ID,
       'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
     },
     responseType: 'json',
     timeout: 1000, // 1 second timeout
-    data: body
+    data: req.body.query
   };
 
   try {
     const response = await axios.request(config);
 
-    console.log(response.data);
     res.json(response.data);
   } catch(err) {
     console.error(err);
