cccd7d3c21eda69cc4b545230a7ca264c5f4422b
matiasc18 <matiascarulli@gmail.com>
2022-07-01 12:41:52 -0400

    Began responsive header/nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaPowerOff } from 'react-icons/fa';
 import '../styles/header.css';
 
 const Header = () => {
   return (
-    <header>
-      <Link to='/' id="header-title"><code>{`<frontloggd>`}</code></Link>
-      <nav className="nav-links">
-        <Link to='/games'>Games</Link>
-        <Link to='/signup'>Sign Up</Link>
-        <Link to='/'>Log In</Link>
-      </nav>
-    </header>
+    <div id="header-container">
+      <header>
+        <Link to="/" id="header-title"><code>{`<frontloggd>`}</code></Link>
+        <FaPowerOff size="1.5em" className="menu-button" />
+        <nav className="nav-links">
+          <Link className="header-link" to="/games">Games</Link>
+          <Link className="header-link" to="/">Log In</Link>
+          <Link className="header-link" to="/signup">Sign Up</Link>
+        </nav>
+      </header>
+    </div>
   )
 }
 
 export default Header;
+
+// * Potential icons
+// * FaAngleDown (Down, Left, Up, Right)
+// * FaAngleDoubleUp (Down, Left, Up, Right)
+// * FaBars
+// * FaChevronRight (Down, Left, Up, Right)
+// * FaEllipsisH (H, V)
+// * FaPowerOff
+// * FaSlidersH
+// * FaSortDown (Down, Up)
