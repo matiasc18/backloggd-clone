5223d68578413bea678505709f6d311b04b970f9
matiasc18 <matiascarulli@gmail.com>
2022-07-07 21:06:14 -0400

    Finished pagination + styles

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,60 +1,84 @@
 import React from 'react';
 import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+import '../styles/pagination.css';
 
 const Pagination = ({ gamesPerPage, totalGames, currentPage, updatePage }) => {
 
   // Get maximum page number
   const totalPages = Math.floor(totalGames / gamesPerPage);
+  // JSX to be inserted (array of game list items)
+  const insertion = [];
 
-  // TODO Fix to work properly with totalGames count
-  if (currentPage < 3) {
+  //* List start
+  //? Apperance when on pages 1 thru 3
+  if (currentPage <= 3) {
+    for (let i = 1; i <= 4; i++) {
+      if (i === currentPage)
+        insertion.push(<li className="page-selector active-page">{i}</li>);
+      else 
+        insertion.push(<li className="page-selector" onClick={() => updatePage(i)}>{i}</li>);
+    }
+
     return (
       <div className="pagination">
         <ul className="page-list">
-          {currentPage !== 1 ? <li className="page-selector" onClick={() => updatePage(currentPage - 1)}><FaChevronLeft /></li> : <></>}
-          <li className="page-selector" onClick={() => updatePage(1)}>1</li>
-          <li className="page-selector" onClick={() => updatePage(2)}>2</li>
-          <li className="page-selector" onClick={() => updatePage(3)}>3</li>
+          {currentPage !== 1 ? <li className="page-arrow" onClick={() => updatePage(currentPage - 1)}><FaChevronLeft /></li> : <></>}
+          {insertion}
           <li>...</li>
           <li className="page-selector" onClick={() => updatePage(totalPages)}>{totalPages}</li>
-          <li className="page-selector" onClick={() => updatePage(currentPage + 1)}><FaChevronRight /></li>
+          <li className="page-arrow" onClick={() => updatePage(currentPage + 1)}><FaChevronRight /></li>
         </ul>
       </div>
     )
   }
 
-  else if (currentPage >= 3 && currentPage <= 9) {
+  //* Mid list
+  //? Apperance when on pages 4 thru (total - 3))
+  else if (currentPage >= 4 && currentPage <= totalPages - 3) {
+    for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+      if (i === currentPage)
+        insertion.push(<li className="page-selector active-page">{i}</li>);
+      else 
+        insertion.push(<li className="page-selector" onClick={() => updatePage(i)}>{i}</li>);
+    }
+
     return (
       <div className="pagination">
         <ul className="page-list">
-          <li className="page-selector" onClick={() => updatePage(currentPage - 1)}><FaChevronLeft /></li>
+          <li className="page-arrow" onClick={() => updatePage(currentPage - 1)}><FaChevronLeft /></li>
           <li className="page-selector" onClick={() => updatePage(1)}>1</li>
-          <li>...</li>
-          <li className="page-selector" onClick={() => updatePage(currentPage - 1)}>{ currentPage - 1 }</li>
-          <li className="page-selector">{ currentPage }</li>
-          <li className="page-selector" onClick={() => updatePage(currentPage + 1)}>{ currentPage + 1 }</li>
-          <li>...</li>
+          <li className="page-dots">...</li>
+          { insertion }
+          <li className="page-dots">...</li>
           <li className="page-selector" onClick={() => updatePage(totalPages)}>{totalPages}</li>
-          <li className="page-selector" onClick={() => updatePage(currentPage + 1)}><FaChevronRight /></li>
+          <li className="page-arrow" onClick={() => updatePage(currentPage + 1)}><FaChevronRight /></li>
         </ul>
       </div>
     )
   }
 
-  return (
-    <div className="pagination">
-      <ul className="page-list">
-        <li className="page-selector" onClick={() => updatePage(currentPage - 1)}><FaChevronLeft /></li>
-        <li onClick={() => updatePage(1)} className="page-selector">1</li>
-        <li>...</li>
-        <li onClick={() => updatePage(totalPages - 3)} className="page-selector">{ totalPages - 3}</li>
-        <li onClick={() => updatePage(totalPages - 2)} className="page-selector">{ totalPages - 2}</li>
-        <li onClick={() => updatePage(totalPages - 1)} className="page-selector">{ totalPages - 1}</li>
-        <li onClick={() => updatePage(totalPages)} className="page-selector">{ totalPages }</li>
-        {currentPage !== totalPages ? <li onClick={() => updatePage(currentPage + 1)} className="page-selector"><FaChevronRight /></li> : <></>}
-      </ul>
-    </div>
-  )
+  //* List ending
+  //? Apperance when on pages (total - 2) thru total pages
+  else {
+    for (let i = totalPages - 3; i <= totalPages; i++) {
+      if (i === currentPage)
+        insertion.push(<li className="page-selector active-page">{i}</li>);
+      else 
+        insertion.push(<li className="page-selector" onClick={() => updatePage(i)}>{i}</li>);
+    }
+
+    return (
+      <div className="pagination">
+        <ul className="page-list">
+          <li className="page-arrow" onClick={() => updatePage(currentPage - 1)}><FaChevronLeft /></li>
+          <li onClick={() => updatePage(1)} className="page-selector">1</li>
+          <li className="page-dots">...</li>
+          { insertion }
+          {currentPage !== totalPages ? <li onClick={() => updatePage(currentPage + 1)} className="page-arrow"><FaChevronRight /></li> : <></>}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Pagination;
