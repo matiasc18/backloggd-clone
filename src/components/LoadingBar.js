53caa78a2ce2e388487b43a9f792ec3c91f27ea6
matiasc18 <matiascarulli@gmail.com>
2022-07-07 21:06:04 -0400

    Added loading bar
    https://codepen.io/stoepke/pen/QOOqGW

---
+++ src/components/LoadingBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/loadingBar.css';
+
+const LoadingBar = () => {
+  return (
+    <div className="loader-container">
+      <div class="loader">
+        <div class="loaderBar"></div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingBar;
