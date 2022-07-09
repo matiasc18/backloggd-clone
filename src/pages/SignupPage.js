e8512bc4f6f1a511e4bd5e9927117b1a5dffa20b
matiasc18 <matiascarulli@gmail.com>
2022-07-08 22:21:35 -0400

    Finished signup page design

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -3,40 +3,31 @@ import '../styles/signupPage.css';
 
 const SignupPage = () => {
   return (
-    <main id="signup-page">
-      <div id="signup-container">
-        <h1 id="signup-title">Sign Up</h1>
-        <hr />
-        <form action="#" id="signup-form">
-          {/* First Name and Last Name */}
-          <div className="double-input">
-            <div>
-              <label for="first-name" className="form-label">First Name</label>
-              <input className="signup-input" id="first-name" type="text"/>
-            </div>
-            <div>
-              <label for="last-name" className="form-label">Last Name</label>
-              <input className="signup-input" id="last-name" type="text"/>
-            </div>
+    <div id="signup-page">
+      <div id="form-container">
+        <h1 id="signup-title">Register</h1>
+        <form id="signup-form" autoComplete="off">
+          <div className="input-group">
+            <input id="username" className="signup-input" type="" placeholder="Username" required/>
+            <label htmlFor="username" className="signup-label">Username</label>
           </div>
-          {/* Email */}
-          <div>
-            <label for="email" className="form-label">Email</label>
-            <input className="signup-input" id="email" type="email"/>
+          <div className="input-group">
+            <input id="email" className="signup-input" type="email" placeholder="Email" autoComplete="on" required/>
+            <label htmlFor="email" className="signup-label">Email</label>
           </div>
-          {/* Password and Confirm Password */}
-          <div>
-            <label for="password" className="form-label">Password</label>
-            <input className="signup-input" id="password" type="password"/>
+          <div className="input-group">
+            <input id="password" className="signup-input" type="password" placeholder="Password" required/>
+            <label htmlFor="password" className="signup-label">Password</label>
           </div>
-          <div>
-            <label for="confirm-password" className="form-label">Confirm Password</label>
-            <input className="signup-input" id="confirm-password" type="password"/>
+          <div className="input-group">
+            <input id="confirmPassword" className="signup-input" type="password" placeholder="Confirm Password" required/>
+            <label htmlFor="confirmPassword" className="signup-label">Confirm Password</label>
           </div>
+          <button id="signup-submit" type="submit" form="signup-form">Submit</button>
+          <span>Already have an account? <a href="/login">Log In</a></span>
         </form>
-        <button className="form-button" type="submit" form="signup-form" value="Submit">Submit</button>
       </div>
-    </main>
+    </div>
   )
 }
 
