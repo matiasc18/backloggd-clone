76000760cbb756c8c8aac95a1c975c22419d91ae
matiasc18 <matiascarulli@gmail.com>
2022-07-14 18:06:15 -0400

    Added TODO comment

--- backend/routes/igdb.routes.js
+++ backend/routes/igdb.routes.js
@@ -3,6 +3,7 @@ const { getGames } = require('../controllers/gamesController');
 
 // * GET: Get all games
 //TODO change to GET instead of POST
+//TODO Change query to be in params rather than in body
 router.route('/').post(getGames);
 
 module.exports = router;
