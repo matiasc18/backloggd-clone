c18bd1210e69000ba1be825301c61cad17d039cc
matiasc18 <matiascarulli@gmail.com>
2022-07-18 16:45:34 -0400

    Added dashboard (logged in page)

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Header from './components/Header';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
 
+// If something breaks, try putting className="App" outside of BrowserRouter (like it was before)
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/signup' element={<SignupPage />} />
-          <Route path='/login' element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <Header />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div style={{marginTop: '1em', fontSize: '4em', color: 'white', display: 'flex', justifyContent: 'center'}}>Dashboard</div>
+  )
+}
+
+export default Dashboard;
