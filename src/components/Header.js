c94a6aa68d7e4eccb7becf0077d1f8b44a3c6075
matiasc18 <matiascarulli@gmail.com>
2022-07-18 16:45:25 -0400

    Implemented redux logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 import { FaPowerOff } from 'react-icons/fa';
 import '../styles/header.css';
 
+//TODO Change links to a ul with li of Link
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  };
+
   return (
     <div id="header-container">
       <header>
         <Link to="/" id="header-title"><code>{`<frontloggd>`}</code></Link>
         <FaPowerOff size="1.5em" className="menu-button" />
         <nav className="nav-links">
-          <Link className="header-link" to="/games">Games</Link>
-          <Link className="header-link" to="/login">Log In</Link>
-          <Link className="header-link" to="/signup">Sign Up</Link>
+          {user ? (
+            <button className="logout-button" onClick={onLogout}>Logout</button>
+          ) : (
+            <>
+              <Link className="header-link" to="/games">Games</Link>
+              <Link className="header-link" to="/login">Log In</Link>
+              <Link className="header-link" to="/signup">Sign Up</Link>
+            </>)}
         </nav>
       </header>
     </div>
