de2aeb0cc49e58e815b092fd181b06d3a6b32e51
matiasc18 <matiascarulli@gmail.com>
2022-07-22 01:17:44 -0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.AXIOS_BASE_URL,
+  baseURL: 'http://localhost:3500',
   headers: {
     'Content-Type': 'application/json',
   },
