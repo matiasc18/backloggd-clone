48afecc0651ad1658290ab7a3a504ea5437bdf4d
matiasc18 <matiascarulli@gmail.com>
2022-07-25 20:57:57 -0400

    Added mobile menu animation
    + Move menu in from the left
    + Move home-page alongside it
    + Set body position to fixed (can't scroll while in menu)
    + Click hamburger menu OR on the home-page to exit menu

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,28 +19,37 @@ const Header = () => {
     navigate('/');
   };
 
-  const hideMenu = () => {
-    setShowMobileMenu(false);
+  const toggleMenu = () => {
+    if (showMobileMenu) {
+      setShowMobileMenu(false);
+      document.body.style.position = 'static';
+      document.getElementById('home-page').style.marginRight = 0;
+    } else {
+        setShowMobileMenu(true);
+        document.body.style.position = 'fixed';
+        document.getElementById('home-page').style.transition = '0.4s';
+        document.getElementById('home-page').style.marginRight = '100%';
+    }
   };
 
   return (
     <div id="header-container">
       <header>
         <Link to="/" id="header-title">frontloggd</Link>
-        <FaPowerOff size="1.5em" className="menu-button" onClick={() => setShowMobileMenu(!showMobileMenu)}/>
+        <FaPowerOff size="1.5em" className="menu-button" onClick={toggleMenu}/>
         {/* If the menu is active, display mobile menu nav */}
-        { showMobileMenu && <div className="menu-mask" onClick={() => setShowMobileMenu(!showMobileMenu)}></div>}
+        { showMobileMenu && <div className="menu-mask" onClick={toggleMenu}></div>}
         <nav className={showMobileMenu ? "nav-links isActive" : "nav-links"}>
           {user ? (
             <>
-              <Link className="nav-link" to="/games" onClick={hideMenu}>Games</Link>
+              <Link className="nav-link" to="/games" onClick={toggleMenu}>Games</Link>
               <button className="logout-button" onClick={onLogout}>Logout</button>
             </>
           ) : (
             <>
-              <Link className="nav-link" to="/games" onClick={hideMenu}>Games</Link>
-              <Link className="nav-link" to="/login" onClick={hideMenu}>Log In</Link>
-              <Link className="nav-link" to="/signup" onClick={hideMenu}>Sign Up</Link>
+              <Link className="nav-link" to="/games" onClick={toggleMenu}>Games</Link>
+              <Link className="nav-link" to="/login" onClick={toggleMenu}>Log In</Link>
+              <Link className="nav-link" to="/signup" onClick={toggleMenu}>Sign Up</Link>
             </>)}
         </nav>
       </header>
