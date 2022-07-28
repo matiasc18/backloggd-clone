bed25bac66c530c7465651b03a33df90682a6be3
matiasc18 <matiascarulli@gmail.com>
2022-07-28 16:08:48 -0400

    Began user profile screen (dashboard)

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,22 +1,53 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import Games from '../components/Games';
+import { getFavorites, getUserGames, reset } from '../features/games/gameSlice.js';
+import '../styles/dashboard.css';
 
 const Dashboard = () => {
   // For re-routing
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
-  console.log(user);
-
+  const { games, favorites, isError, message } = useSelector((state) => state.games);
+  
   useEffect(() => {
     if (!user) {
       navigate('/games');
     }
-  }, [user, navigate]);
+
+    dispatch(getFavorites());
+    dispatch(getUserGames());
+
+    return () => {
+      dispatch(reset());
+    }
+  }, [user, navigate, isError, message, dispatch]);
+
   if (user) return (
-    <div style={{marginTop: '1em', fontSize: '4em', color: 'white', display: 'flex', justifyContent: 'center'}}>
-      <span>Hello, { user.username }</span>
+    <div className="dashboard">
+      { user && 
+        <div id="bio">
+          <h2 className="dashboard-title">Bio</h2>
+          <hr />
+          <span>{user.username}</span>
+        </div> }
+      { favorites && 
+        <div id="favorites-container">
+          <h2 className="dashboard-title">Favorites</h2>
+          <hr />
+          <Games games={favorites} />
+        </div>
+      }
+      { games && 
+        <div id="dashboard-games-container">
+          <h2 className="dashboard-title">Games</h2>
+          <hr />
+          <Games games={games} />
+        </div>
+      }
     </div>
   )
 }
