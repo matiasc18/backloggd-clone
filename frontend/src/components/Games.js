07438b08dfee00af808cbe1bd0183a991d3aae09
matiasc18 <matiascarulli@gmail.com>
2022-08-02 19:23:38 -0400

    Removed getRatingColor func

--- frontend/src/components/Games.js
+++ frontend/src/components/Games.js
@@ -1,40 +1,26 @@
 import React, { useEffect } from 'react';
 import { imgPath } from '../api/utils';
 import { useDispatch } from 'react-redux';
-import { addGames } from '../features/games/gameSlice.js';
-// import gamesStyles from '../styles/games.module.css';
+import { useNavigate } from 'react-router-dom';
+import { getRatingColor } from '../api/utils';
 
 //TODO Optimize game loading
 //TODO - Fetch all games at once + iterate through locally, rather than fetch every 30 games  
 //* Render grid of game cards
 const Games = ({ games, list }) => {
+  // For re-routing
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  //? Apply correct color based on game rating
-  const getRatingColor = (rating) => {
-    if (rating >= 95)
-      return { backgroundColor: 'gold', color: 'black' };
-    else if (rating >= 90)
-      return { color: 'lime' };
-    else if (rating >= 80)
-      return { color: 'limegreen' };
-    else if (rating >= 70)
-      return { color: 'yellow' };
-    else if (rating >= 60)
-      return { color: 'orange' };
-    else
-      return { color: 'crimson' };
-  }
-
-  const addGame = (game) => {
-    dispatch(addGames([game]));
+  const openGame = (game) => {
+    navigate(`/games/${game.id}`);
   };
 
   // Display game cards + page selector (bottom)
   return (
     <>
       { games && games.map((game) => (
-        <div key={(game.id) + list} className="game-card" onClick={() => (window.location.pathname === '/games') ? addGame(game) : console.log('Cannot add game')}>
+        <div key={(game.id) + list} className="game-card" onClick={() => openGame(game)}>
           <img className="game-cover" src={`${imgPath}/${game.cover.image_id}.jpg`} alt={`Cover art for ${game.name}`} />
           <div className="game-info">
             <span className="game-title">{game.name}</span>
