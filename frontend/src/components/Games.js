f181096f2df265aa2e70d40e954ac962f8f50679
matiasc18 <matiascarulli@gmail.com>
2022-08-05 20:50:40 -0400

    Shortened game titles when too long

--- frontend/src/components/Games.js
+++ frontend/src/components/Games.js
@@ -1,29 +1,41 @@
 import React, { useEffect } from 'react';
 import { imgPath } from '../api/utils';
 import { useDispatch } from 'react-redux';
+import { reset } from '../features/games/gameSlice.js';
 import { useNavigate } from 'react-router-dom';
 import { getRatingColor } from '../api/utils';
 
-//TODO Optimize game loading
-//TODO - Fetch all games at once + iterate through locally, rather than fetch every 30 games  
-//* Render grid of game cards
+//* Render game cards
 const Games = ({ games, list }) => {
   // For re-routing
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  // Reset game state and redirect to gameExpanded component
   const openGame = (game) => {
+    dispatch(reset());
     navigate(`/game-details/${game.id}`);
   };
 
-  // Display game cards + page selector (bottom)
+  const checkLength = (title) => {
+    let modifiedTitle = '';
+    if (title.length >= 30) {
+      modifiedTitle = title.substring(0, 16);
+      modifiedTitle += ' ... ';
+      modifiedTitle += title.substring(title.length - 10);
+      return modifiedTitle;
+    }
+    return title;
+  };
+
+  // Display game cards
   return (
     <>
-      { games && games.map((game) => (
+      {games && games.map((game) => (
         <div key={(game.id) + list} className="game-card" onClick={() => openGame(game)}>
           <img className="game-cover" src={`${imgPath}/${game.cover.image_id}.jpg`} alt={`Cover art for ${game.name}`} />
           <div className="game-info">
-            <span className="game-title">{game.name}</span>
+            <span className="game-title">{checkLength(game.name)}</span>
             <span className="game-rating" style={getRatingColor(Math.floor(game.rating))}>{Math.floor(game.rating)}</span>
           </div>
         </div>
