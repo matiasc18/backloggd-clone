f30e8ebec08707660d66baeaf70467dac44a9c14
matiasc18 <matiascarulli@gmail.com>
2022-10-23 21:51:18 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ app.use('/games', require('./routes/games.routes'));
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
-
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
 } else {
   // Catch for request to nonexistant page
