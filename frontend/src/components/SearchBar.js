e5fb8e24070ac12083c82f6fd3cf80662e97d81c
matiasc18 <matiascarulli@gmail.com>
2022-10-27 01:52:52 -0400

    Cleaned up code

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const SearchBar = ({ resetHeader }) => {
+const SearchBar = () => {
   const [search, setSearch] = useState('');
   const navigate = useNavigate();
 
@@ -15,64 +15,9 @@ const SearchBar = ({ resetHeader }) => {
     }
   };
 
-  useEffect(() => {
-    if (search)
-      console.log(search);
-  }, [search]);
-
   return (
-    <>
-      <input placeholder="Search" type="text" id="search" onSubmit={handleSearch} onChange={(e) => setSearch(e.target.value)} onKeyDown={handleSearch}/>
-    </>
+    <input placeholder="Search" type="text" id="search" onSubmit={handleSearch} onChange={(e) => setSearch(e.target.value)} onKeyDown={handleSearch} />
   )
 }
 
 export default SearchBar;
-
-// import React, { useState, useEffect, useMemo } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { searchGames, reset } from '../features/games/gameSlice.js';
-
-// const SearchBar = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const { searchedGames, isSuccess } = useSelector((state) => state.game);
-
-//   const [search, setSearch] = useState('');
-//   const [searched, setSearched] = useState(false);
-//   let paramSearch = '';
-
-//   const handleSearch = (e) => {
-//     if (e.key === 'Enter')
-//     {
-//       paramSearch = search.replaceAll(' ', '-');
-//       dispatch(searchGames(paramSearch));
-//       setSearched(true);
-//       e.target.value = '';
-//       setSearch('');
-//     }
-//   };
-
-//   useEffect(() => {
-//     console.log(search);
-//   }, [search]);
-
-//   useEffect(() => {
-//     if (isSuccess && searched && searchedGames) {
-//       const games = searchedGames;
-//       dispatch(reset());
-//       navigate(`/search/${paramSearch}`, { searchedGames: games, searchQuery: search });
-//     }
-
-//   }, [searchedGames, isSuccess]);
-
-//   return (
-//     <>
-//       <input type="text" id="search" onSubmit={handleSearch} onChange={(e) => setSearch(e.target.value)} onKeyDown={handleSearch}/>
-//     </>
-//   )
-// }
-
-// export default SearchBar;
