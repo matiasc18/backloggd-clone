a0847727919e3d4c4c9d726a8e95f4e589a30a9b
matiasc18 <matiascarulli@gmail.com>
2022-10-29 01:59:51 -0400

    Added profile navigation + component-ified
    - Added nav for selecting different profile sections
    - Turned bio, games and favorites section into Profile component, will do with the rest

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,23 +1,30 @@
-import { useEffect, useMemo, useState } from 'react';
+import { getUser } from '../features/user/userSlice.js';
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getUser, getFavorites, getUserGames } from '../features/user/userSlice.js';
-import Games from '../components/Games';
 import Pagination from '../components/Pagination';
+import Games from '../components/Games';
+import Profile from '../components/Profile.js';
+
+const pageSelector = (user, userInfo, page) => {
+  switch (page) {
+    case 'profile':
+      return <Profile user={user} userInfo={userInfo} />
+    break;
+  }
+  return <span>Coming soon!</span>
+}
 
 // User profile screen
 const Dashboard = () => {
   // For re-routing / redux dispatch
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [currentPage, setCurrentPage] = useState(1);
-  
+
   const { user } = useSelector((state) => state.auth);
-  const { userInfo, games, favorites } = useSelector((state) => state.user);
-  const displayedGames = useMemo(() => {
-    if (games)
-      return games.results.slice((currentPage - 1) * 30, currentPage * 30);
-  }, [games]);
+  const { userInfo } = useSelector((state) => state.user);
+
+  const [page, setPage] = useState('profile');
 
   // Runs whenever redux auth state changes
   useEffect(() => {
@@ -25,54 +32,31 @@ const Dashboard = () => {
       navigate('/');
       return;
     }
-    dispatch(getUserGames());
-    dispatch(getFavorites());
     dispatch(getUser());
   }, [user, navigate, dispatch]);
-
-  //? Updates current page of results
-  const updatePage = async (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
 
   return (
     <main id="dashboard">
       {user && userInfo && <>
+        <h2 id="user-name">{user.username}</h2>
+        <section id="dashboard-nav">
+          <ul id="dashboard-nav-links">
+            <label><input type="radio" onClick={() => setPage('profile')}/>Profile</label>
+            <label><input type="radio" onClick={() => setPage('games')}/>Games</label>
+            <label><input type="radio" onClick={() => setPage('reviews')}/>Reviews</label>
+            <label><input type="radio" onClick={() => setPage('lists')}/>Lists</label>
+            <label><input type="radio" onClick={() => setPage('friends')}/>Friends</label>
+          </ul>
+          <button className="edit-profile">Edit Profile</button>
+        </section>
         <section id="bio">
-          <h2>{user.username}</h2>
+          <h2>Bio</h2>
           <hr />
           <span className="bio-details"><strong>Joined</strong> {userInfo.dateJoined}</span>
           <span className="bio-details"><strong>Games:</strong> {userInfo.totalGames}</span>
           <span className="bio-details"><strong>Favorites:</strong> {userInfo.totalFavorites}</span>
         </section>
-        <section id="dashboard-favorites">
-          <h2>Favorites</h2>
-          <hr />
-          <div id="user-favorites">
-            {favorites && <Games games={favorites.results} list={1} />}
-          </div>
-        </section>
-        <section id="dashboard-games">
-          <h2>Games</h2>
-          <hr />
-          {games &&
-            <Pagination
-              gamesPerPage={30}
-              totalGames={games.totalGames}
-              currentPage={currentPage}
-              updatePage={updatePage}
-            />}
-          <div id="user-games">
-            {displayedGames && <Games games={displayedGames} list={2} />}
-          </div>
-          {games &&
-            <Pagination
-              gamesPerPage={30}
-              totalGames={games.totalGames}
-              currentPage={currentPage}
-              updatePage={updatePage}
-            />}
-        </section>
+        {pageSelector(user, userInfo, page)}
       </>}
     </main>
   )
