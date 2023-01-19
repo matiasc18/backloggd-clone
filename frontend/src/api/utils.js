2264176b4423fb5f4e8d156d8cc9468e46b66bfa
matiasc18 <matiascarulli@gmail.com>
2023-01-18 19:03:21 -0500

    Added search queries
    - Coming Soon list
    - Popular this Month list
    - Trending list

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -1,14 +1,4 @@
 // ? Default IGDB query (trending page)
-export const defaultQuery = {
-  fields: 'name, cover.image_id, rating',
-  filter: 'rating > 78 & rating != null & cover != null & hypes != null & first_release_date != null & genres.name != null & first_release_date != null & summary != null & category = (0,3,6,8,9,10,11)',
-  sort: 'hypes desc',
-  limit: 500,
-  page: 1,
-  search: '',
-}
-
-// ? Default IGDB query (trending page)
 export const detailsQuery = {
   fields: 'name, cover.image_id, rating, first_release_date, genres.name, screenshots.image_id, summary, involved_companies.company.name, involved_companies.company.websites.url, involved_companies.company.developed, involved_companies.developer, category, aggregated_rating, aggregated_rating_count, expanded_games, dlcs, expansions, external_games.category, external_games.url, follows, franchise, hypes, parent_game, platforms.platform_logo.image_id, platforms.name, rating_count, slug, status, storyline, tags, total_rating, total_rating_count, websites.category, websites.url, version_parent, videos.video_id, age_ratings.category, age_ratings.rating, age_ratings.rating_cover_url, artworks.width, artworks.height, artworks.image_id, collection.name, game_modes.name, similar_games',
   filter: '',
@@ -62,4 +52,38 @@ export const getRatingColor = (rating) => {
     return { color: 'orange' };
   else
     return { color: 'crimson' };
+}
+
+const MONTH = 2592000
+const getCurrentDate = () => {
+  return Math.floor(Date.now() / 1000);
+} 
+
+export const trendingQuery = {
+  fields: 'name, cover.image_id, rating',
+  filter: 'rating != null & rating > 78 & cover != null & hypes != null & first_release_date != null & genres.name != null & first_release_date != null & summary != null & category = (0,3,6,8,9,10,11)',
+  sort: 'hypes desc',
+  limit: 500,
+  page: 1,
+  search: '',
+}
+
+export const comingSoonQuery = {
+  fields: 'name, cover.image_id, first_release_date',
+  filter: `cover != null & first_release_date != null & first_release_date >= ${getCurrentDate()} & first_release_date <= ${(3*MONTH) + getCurrentDate()}`,
+  sort: 'first_release_date asc',
+  limit: 3,
+  page: 1,
+  search: ''
+}
+
+// & first_release_date >= ${Math.floor(Date.now() / 1000)} & first_release_date <= ${(3*2592000) + Math.floor(Date.now() / 1000)}
+
+export const popularThisMonthQuery = {
+  fields: 'name, cover.image_id, rating',
+  filter: 'rating > 78 & rating != null & cover != null & hypes != null & first_release_date != null & genres.name != null & first_release_date != null & summary != null & category = (0,3,6,8,9,10,11)',
+  sort: 'hypes desc',
+  limit: 5,
+  page: 1,
+  search: '',
 }
