1744996dee42b2d3806aa59f0d03d446df68526a
matiasc18 <matiascarulli@gmail.com>
2023-02-06 02:17:13 -0500

    Removed redux gameSlice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import userReducer from '../features/user/userSlice.js';
-import gameReducer from '../features/games/gameSlice.js';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
-    game: gameReducer
   },
 });
