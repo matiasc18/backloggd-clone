93d07c73007eac7346a94995ad9f92f57cc77385
matiasc18 <matiascarulli@gmail.com>
2023-02-06 02:23:39 -0500

    Removed addGames, addFavs, getFavs

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -3,8 +3,6 @@ import userService from './userService';
 
 const initialState = {
   userInfo: null,
-  games: null,
-  favorites: null,
   isError: false,
   isSuccess: false,
   isLoading: false,
@@ -16,54 +14,6 @@ export const getUser = createAsyncThunk('user/info', async (_, thunkAPI) => {
   try {
     const accessToken = thunkAPI.getState().auth.user.accessToken;
     return await userService.getUser(accessToken);
-  } catch (err) {
-    // Or return server error message in payload
-    const message = err.response.data.error || err.message || err.toString();
-    return thunkAPI.rejectWithValue(message);
-  }
-});
-
-//? Add game to user backlog
-export const addGames = createAsyncThunk('user/addGames', async (gameData, thunkAPI) => {
-  try {
-    const accessToken = thunkAPI.getState().auth.user.accessToken;
-    return await userService.addGames(gameData, accessToken);
-  } catch (err) {
-    // Or return server error message in payload
-    const message = err.response.data.error || err.message || err.toString();
-    return thunkAPI.rejectWithValue(message);
-  }
-});
-
-//? Get all of the user's games
-export const getUserGames = createAsyncThunk('user/allGames', async (_, thunkAPI) => {
-  try {
-    const accessToken = thunkAPI.getState().auth.user.accessToken;
-    return await userService.getUserGames(accessToken);
-  } catch (err) {
-    // Or return server error message in payload
-    const message = err.response.data.error || err.message || err.toString();
-    return thunkAPI.rejectWithValue(message);
-  }
-});
-
-//? Add game to user backlog
-export const addFavorite = createAsyncThunk('user/addFavorite', async (gameData, thunkAPI) => {
-  try {
-    const accessToken = thunkAPI.getState().auth.user.accessToken;
-    return await userService.addFavorite(gameData, accessToken);
-  } catch (err) {
-    // Or return server error message in payload
-    const message = err.response.data.error || err.message || err.toString();
-    return thunkAPI.rejectWithValue(message);
-  }
-});
-
-//? Get all of the user's favorite games
-export const getFavorites = createAsyncThunk('user/favorites', async (_, thunkAPI) => {
-  try {
-    const accessToken = thunkAPI.getState().auth.user.accessToken;
-    return await userService.getFavorites(accessToken);
   } catch (err) {
     // Or return server error message in payload
     const message = err.response.data.error || err.message || err.toString();
@@ -95,64 +45,6 @@ export const userSlice = createSlice({
         state.userInfo = action.payload
       })
       .addCase(getUser.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-      })
-      //* Add game
-      .addCase(addGames.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(addGames.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.games = action.payload.games
-        state.message = action.payload.message
-      })
-      .addCase(addGames.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-      })
-      //* Get all user games
-      .addCase(getUserGames.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(getUserGames.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.games = action.payload
-      })
-      .addCase(getUserGames.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-      })
-      //* Add favorite
-      .addCase(addFavorite.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(addFavorite.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.favorites = action.payload.favorites
-        state.message = action.payload.message
-      })
-      .addCase(addFavorite.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-      })
-      //* Get all user favorite games
-      .addCase(getFavorites.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(getFavorites.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.favorites = action.payload
-      })
-      .addCase(getFavorites.rejected, (state, action) => {
         state.isLoading = false
         state.isError = true
         state.message = action.payload
