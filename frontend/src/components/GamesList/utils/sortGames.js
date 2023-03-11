5ac9b2b57f8028bbff1ec7f0e5816081e08d4ab3
matiasc18 <matiascarulli@gmail.com>
2023-03-11 12:59:51 -0500

    Changed sort names

--- frontend/src/components/GamesList/utils/sortGames.js
+++ frontend/src/components/GamesList/utils/sortGames.js
@@ -1,12 +1,12 @@
 export const sortGames = (games, sort) => {
   switch (sort) {
-    case 'rating':
+    case 'Rating':
       return [...games].sort((game1, game2) => (game1.rating < game2.rating) ? 1 : ((game1.rating > game2.rating) ? -1 : 0));
-    case 'a-z':
+    case 'A-Z':
       return [...games].sort((game1, game2) => game1.name.localeCompare(game2.name));
-    case 'z-a':
+    case 'Z-A':
       return [...games].sort((game1, game2) => game2.name.localeCompare(game1.name));
-    case 'release':
+    case 'Release':
       return [...games].sort((game1, game2) => game2.first_release_date - game1.first_release_date);
     case 'default':
       return games;
