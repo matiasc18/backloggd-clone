c3766fe437a915fa95ba3fee1e969d82e763e6cf
matiasc18 <matiascarulli@gmail.com>
2023-03-11 12:55:36 -0500

    Updated user games model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -27,27 +27,73 @@ const userSchema = new mongoose.Schema({
     trim: true
   },
   // Users list of games (game_id)
-  games: {
-    type: [Number],
-    required: true,
-    default: []
-  },
+  games: [
+    {
+      id: Number,
+      status: {
+        minsPlayed: Number, // in minutes
+        completionStatus: Number, // 1: played, 2: completed, 3: retired, 4: shelved, 5: abandoned
+        gameStatus: Number, // 1: played, 2: playing, 3: backlog, 4: wishlist
+        library: [
+          {
+            platform: Number,
+            ownership: Number, // 1: physical, 2: digital, 3: lost/lost
+          }
+        ],
+      log: [
+        {
+          rating: Number,
+          platform: Number,
+          startDate: Date,
+          finishDate: Date,
+          review: String,
+          spoilerWarning: Boolean,
+          advanced: {
+            medium: Number, // 1: owned, 2: subscription, 3: borrowed, 4: watched
+            minsPlayed: Number, // in minutes
+            mastered: Boolean,
+            replay: Boolean,
+            logTitle: {
+              type: String,
+              default: 'Log'
+            }
+          }
+        }
+      ],
+
+        id: Number,
+        developed: [Number],
+        name: String,
+        websites: [
+          {
+            id: Number,
+            url: String
+          }
+        ]
+      }
+    }
+  ],
+  // games: {
+  //   type: [Number],
+  //   required: true,
+  //   default: []
+  // }
   // Users list of favorites (game_id)
   favorites: {
-    type: [Number],
-    required: true,
-    default: []
-  },
+  type: [Number],
+  required: true,
+  default: []
+},
   totalGames: {
-    type: Number,
-    default: 0,
-    required: false
-  },
+  type: Number,
+  default: 0,
+  required: false
+},
   totalFavorites: {
-    type: Number,
-    default: 0,
-    required: false
-  }
+  type: Number,
+  default: 0,
+  required: false
+}
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
