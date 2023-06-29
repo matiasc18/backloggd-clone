6df9a2d73204cf32020c93dcc9bcd5c065a1a30f
matiasc18 <matiascarulli@gmail.com>
2023-06-29 01:47:47 -0400

    Redesigned mobile navbar
    - Switched side slide out to drop down (similar to Backloggd)
    - Added placeholder buttons for all buttons available on backloggd
    - Added many react-icons

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -4,13 +4,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../features/auth/authSlice';
 import { reset } from '../../features/auth/authSlice';
 import { reset as resetUser } from '../../features/user/userSlice.js';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaBookOpen, FaCog, FaGamepad, FaGift, FaHeart, FaHistory, FaLayerGroup, FaPlay, FaSignOutAlt, FaSignal, FaUser, FaUserFriends, FaPlus } from 'react-icons/fa';
 import { getWindowSize } from '../../api/utils';
 import SearchBar from './utils/SearchBar.js';
+import { ImBooks } from 'react-icons/im';
 
 //TODO Change links to a ul with li of Link
 const Header = () => {
-  const maxMobileWidth = 480;
+  const maxMobileWidth = 960;
 
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -21,11 +22,6 @@ const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   useEffect(() => {
-    // Makes sure menu isn't active on initial render
-    const menu = document.getElementById('nav-links');
-    if (menu.classList.contains === 'is-active')
-      menu.classList.remove('is-active');
-
     // Add event listener for screen size change (runs whenever window size changes)
     window.addEventListener('resize', () => {
       setWindowSize(getWindowSize());
@@ -39,29 +35,6 @@ const Header = () => {
     }
   }, []);
 
-  // Reset the header's transparent effect when not viewing a game's details
-  useEffect(() => {
-    if (!(/^\/game-details\/[0-9]+$/).test(window.location.pathname))
-      resetHeader();
-  }, [window.location.pathname])
-
-  // Close hamburger menu if its open while rotating phone device
-  useEffect((showMobileMenu, toggleMenu) => {
-    if (windowSize.innerWidth >= maxMobileWidth && showMobileMenu) {
-      toggleMenu();
-    }
-  }, [windowSize]);
-
-  // Runs whenever user clicks on hamburger menu button
-  useEffect(() => {
-    const menu = document.getElementById('nav-links');
-
-    if (showMobileMenu)
-      menu.classList.add('is-active');
-    else
-      menu.classList.remove('is-active');
-  }, [showMobileMenu]);
-
   // Logout, reset user state, close the menu (if on mobile), and go to homepage
   const onLogout = () => {
     setShowMobileMenu(false);
@@ -74,63 +47,48 @@ const Header = () => {
   // Opens/closes mobile hamburger menu
   const toggleMenu = () => {
     if (window.innerWidth < maxMobileWidth) {
-      // Current page to be translated left when menu opens
-      const currentPage = document.getElementById('App').children.item(1);
-      if (!showMobileMenu) {
-        setShowMobileMenu(true);
-
-        // Removes page scrolling capability
-        document.body.style.position = 'fixed';
-
-        // Translates the current page left
-        currentPage.style.transition = '0.56s';
-        currentPage.style.transform = 'translateX(-43%)';
-        // Returns the page back to normal after closing menu
-      }
-      else {
-        setShowMobileMenu(false);
-        document.body.style.position = 'static';
-        currentPage.style.transform = 'translateX(0)';
-      }
+      setShowMobileMenu(!showMobileMenu);
     }
-  };
-
-  // Runs whenever user exits expanded game page (removes blur)
-  const resetHeader = () => {
-    document.getElementById('header-container').classList.remove('is-active-game');
-    document.getElementById('nav-links').classList.remove('is-active-game');
-    document.getElementById('nav-links').classList.remove('is-active');
   };
 
   return (
     <>
       <div id="header-container">
         <header>
-          <Link to="/" id="header-title" onClick={resetHeader}>frontloggd</Link>
-          <FaBars size="1.75em" className="menu-button" style={showMobileMenu ? { opacity: 0.7 } : {}} onClick={toggleMenu} />
-          {/* If the menu is active, display mobile menu version of nav */}
-          <div id="menu-mask" className={showMobileMenu ? "is-active" : ""} onClick={toggleMenu}></div>
-          <nav id="nav-links">
-            {/* <div className="blur"></div> */}
-            {user ? (
-              // If the user exists, show the logout button
-              <>
-                <Link className="nav-link" to="/dashboard/profile" onClick={() => toggleMenu()} style={{fontWeight: 'bold', letterSpacing: '0.5px'}}>{user.username}</Link>
-                <Link className="nav-link" to="/games" onClick={() => toggleMenu()}>Games</Link>
-                <SearchBar resetHeader={resetHeader}/>
-                <span className="nav-link" onClick={onLogout}>Logout</span>
-              </>
-            ) : (
-              // Otherwise, show normal nav
-              <>
-                <Link className="nav-link" to="/login" onClick={() => toggleMenu()}>Log In</Link>
-                <Link className="nav-link" to="/signup" onClick={() => toggleMenu()}>Sign Up</Link>
-                <Link className="nav-link" to="/games" onClick={() => toggleMenu()}>Games</Link>
-                <SearchBar />
-              </>)}
+          <Link to="/" id="header-title">frontloggd</Link>
+          <nav>
+            <Link className="nav-button" to="/games" onClick={() => setShowMobileMenu(false)}>Games</Link>
+            <FaBars size="1.75em" className="menu-button" style={showMobileMenu ? { opacity: 0.7 } : {}} onClick={toggleMenu} />
           </nav>
         </header>
-        {/* <div className="blur"></div> */}
+      </div>
+      <div className={`${showMobileMenu ? 'nav-dropdown show-dropdown' : 'nav-dropdown'} ${user ? 'user-dropdown' : ''}`}>
+        {user ? (
+          <div id="user-buttons">
+            <Link className="dropdown-button grid-1" to="/dashboard/profile" onClick={() => setShowMobileMenu(false)}><FaUser size={20} />Profile</Link>
+            <span className="dropdown-button grid-1"><FaCog size={20} />Settings</span>
+            <span className="dropdown-button grid-1" onClick={onLogout}><FaSignOutAlt size={20} />Log Out</span>
+            <span className="dropdown-button grid-2"><FaGamepad size={24} />Played</span>
+            <span className="dropdown-button grid-2"><FaPlay size={20} />Playing</span>
+            <span className="dropdown-button grid-2"><ImBooks size={20} />Backlog</span>
+            <span className="dropdown-button grid-2"><FaGift size={20} />Wishlist</span>
+            <span className="dropdown-button grid-3"><FaBookOpen size={20} />Journal</span>
+            <span className="dropdown-button grid-3"><FaHistory size={20} />Activity</span>
+            <span className="dropdown-button grid-4"><FaSignal size={20} />Reviews</span>
+            <span className="dropdown-button grid-4"><FaLayerGroup size={20} />Lists</span>
+            <span className="dropdown-button grid-4"><FaHeart size={20} />Likes</span>
+            <span className="dropdown-button grid-5"><FaUserFriends size={20} />Following</span>
+            <span className="dropdown-button grid-5"><FaUserFriends size={20} />Followers</span>
+            <SearchBar />
+            <span className="log-game-button"><FaPlus />Log a Game</span>
+          </div>
+        ) : (
+          <>
+            <Link className="dropdown-button" to="/login" onClick={() => setShowMobileMenu(false)}>Log In</Link>
+            <Link className="dropdown-button" to="/signup" onClick={() => setShowMobileMenu(false)}>Register</Link>
+            <SearchBar />
+          </>
+        )}
       </div>
     </>
   )
@@ -147,3 +105,33 @@ export default Header;
 // * FaPowerOff
 // * FaSlidersH
 // * FaSortDown (Down, Up)
+
+// return (
+//   <>
+//     <div id="header-container">
+//       <header>
+//         <Link to="/" id="header-title" onClick={resetHeader}>frontloggd</Link>
+//         <FaBars size="1.75em" className="menu-button" style={showMobileMenu ? { opacity: 0.7 } : {}} onClick={toggleMenu} />
+//         {/* If the menu is active, display mobile menu version of nav */}
+//         <nav id="nav-links">
+//           {user ? (
+//             // If the user exists, show the logout button
+//             <>
+//               <Link className="nav-link" to="/dashboard/profile" onClick={() => toggleMenu()} style={{ fontWeight: 'bold', letterSpacing: '0.5px' }}>{user.username}</Link>
+//               <SearchBar resetHeader={resetHeader} />
+//               <span className="nav-link" onClick={onLogout}>Logout</span>
+//               <Link className="nav-link" to="/games" onClick={() => toggleMenu()}>Games</Link>
+//             </>
+//           ) : (
+//             // Otherwise, show normal nav
+//             <>
+//               <Link className="nav-link" to="/login" onClick={() => toggleMenu()}>Log In</Link>
+//               <Link className="nav-link" to="/signup" onClick={() => toggleMenu()}>Register</Link>
+//               <Link className="nav-link" to="/games" onClick={() => toggleMenu()}>Games</Link>
+//               <SearchBar />
+//             </>)}
+//         </nav>
+//       </header>
+//     </div>
+//   </>
+// )
