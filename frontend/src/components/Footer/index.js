dbb9967ec751ece6c9fd8a731c4c6c789be6144a
matiasc18 <matiascarulli@gmail.com>
2023-07-01 15:23:16 -0400

    Added credit links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/matiasc18" id="github-link" target="_blank" rel="noreferrer"><FaGithub size="1.5em"/></a>
-      <a href="https://www.linkedin.com/in/matias-carulli-139185236/" id="github-link" target="_blank" rel="noreferrer"><FaLinkedin size="1.5em"/></a>
+      <a href="https://github.com/matiasc18" id="github-link" target="_blank" rel="noreferrer"><FaGithub size="1.5em" /></a>
+      <a href="https://www.linkedin.com/in/matias-carulli-139185236/" id="github-link" target="_blank" rel="noreferrer"><FaLinkedin size="1.5em" /></a>
       <Link to="/about-frontloggd" id="about-frontloggd">About</Link>
-      <span id="igdb-shoutout">IGDB</span>
-      <span id="backloggd-clone">Not my original concept, merely a clone of
-        <a href="https://www.backloggd.com/" target="_blank" rel="noreferrer"> Backloggd</a>
+      <a href="https://www.igdb.com/" target="_blank" rel="noreferrer" id="igdb-shoutout">IGDB</a>
+      <span id="backloggd-clone">Heavily inspired by
+        <a href="https://www.backloggd.com/" target="_blank" rel="noreferrer"> Backloggd</a> &
+        <a href="https://ggapp.io/" target="_blank" rel="noreferrer"> GG</a>
       </span>
     </footer>
   )
