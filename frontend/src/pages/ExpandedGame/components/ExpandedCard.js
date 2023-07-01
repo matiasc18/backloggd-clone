8fa7500de7501e99f9bcc4dcb916f4f4a3f259bc
matiasc18 <matiascarulli@gmail.com>
2023-07-01 15:30:29 -0400

    Edited expanded card layout
    - Made game card larger
    - Separated star rating and logging

--- frontend/src/pages/ExpandedGame/components/ExpandedCard.js
+++ frontend/src/pages/ExpandedGame/components/ExpandedCard.js
@@ -16,16 +16,17 @@ const ExpandedCard = ({ currentGame, refetch }) => {
           <div className="ci-element game-card">
             <img className="game-cover" src={`${imgPath}/${currentGame.cover.image_id}.jpg`} alt={`Cover art for ${currentGame.name}`} />
           </div>
-          <button id="add-game" className="ci-element fl-button" onClick={() => setLogOpen(true)}>Add Game</button>
-          <StarRating />
-          <hr className="ci-element" />
+          <button id="add-game" className="ci-element fl-button" onClick={() => setLogOpen(true)}>Log Game</button>
           <div id="ge-buttons" className="ci-elements">
-            <IoLogoGameControllerA size="4rem" />
-            <FaPlay size="3rem" />
-            <ImBooks size="4rem" />
-            <FaGift size="3rem" />
+            <IoLogoGameControllerA size="5rem" />
+            <FaPlay size="4rem" />
+            <ImBooks size="5rem" />
+            <FaGift size="4rem" />
           </div>
           <button id="add-to-list" className="ci-element fl-button">Add to list</button>
+        </div>
+        <div id="card-interaction">
+          <StarRating />
         </div>
       </div>
       <LogModal currentGame={currentGame} isOpen={isLogOpen} onClose={() => setLogOpen(false)} />
