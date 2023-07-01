fc0c6961631dff5b34b5ecdc692bbcb6cacc842a
matiasc18 <matiascarulli@gmail.com>
2023-07-01 15:24:57 -0400

    Updated landing page
    - Added welcome banner
    - Added quick console links
    - Added background image

--- frontend/src/pages/LandingPage/index.js
+++ frontend/src/pages/LandingPage/index.js
@@ -14,46 +14,66 @@ const LandingPage = () => {
   const { data: comingSoon, error: error2, isError: isError2, isLoading: isLoading2, isSuccess } = useFetchGames('coming-soon');
 
   return (
-    <main id="landing-page">
-      {user &&
-        <section id="lp-header">
-          <div id="lp-header-left">
-            <div className="profile-picture"></div>
-            <div>
-              <Link className="lp-user" to="/dashboard/profile">{user.username}</Link>
+    <>
+      <main id="landing-page">
+        <section id="lp-banner">
+          <h2>Discover new games, expand your collection</h2>
+          <div>
+            <span>Xbox</span>
+            <span>Playstation</span>
+            <span>Switch</span>
+            <span>PC</span>
+          </div>
+          {/* <a className="bg-credits" href="https://www.freepik.com/free-vector/night-ocean-landscape-full-moon-stars-shine_17740155.htm#query=game%20background&position=0&from_view=keyword&track=ais">Image by upklyak</a> on Freepik */}
+        </section>
+        {user &&
+          <section id="lp-header">
+            <div id="lp-header-left">
+              <div className="profile-picture"></div>
               <div>
-                <span className="lp-user-link">Played<span>231</span></span>
-                <span className="lp-user-link">Backlog<span>67</span></span>
-                <span className="lp-user-link">Playing<span>10</span></span>
-                <span className="lp-user-link">Wishlist<span>9</span></span>
+                <Link className="lp-user" to="/dashboard/profile">{user.username}</Link>
+                <div>
+                  <span className="lp-user-link">Played<span>231</span></span>
+                  <span className="lp-user-link">Backlog<span>67</span></span>
+                  <span className="lp-user-link">Playing<span>10</span></span>
+                  <span className="lp-user-link">Wishlist<span>9</span></span>
+                </div>
               </div>
             </div>
+            <div id="lp-header-right">
+              <h1>frontloggd</h1>
+              <span>(Clone website inspired by <a href="https://www.backloggd.com" target="_blank" rel="noopener noreferrer">Backloggd</a>
+                )</span>
+            </div>
+          </section>
+        }
+        <section id="lp-body">
+          <div className="lp-body-left">
+            <section>
+              <h2>Recently logged - <Link to="/games">More</Link></h2>
+              <div className="games-container">
+                {popularThisMonth && <Games games={popularThisMonth.results} list={3} />}
+              </div>
+            </section>
+            <section>
+              <h2>Recently completed - <Link to="/games">More</Link></h2>
+              <section className="games-container">
+                {popularThisMonth && <Games games={popularThisMonth.results} list={3} />}
+              </section>
+            </section>
           </div>
-          <div id="lp-header-right">
-            <h1>frontloggd</h1>
-            <span>(Clone website inspired by <a href="https://www.backloggd.com" target="_blank" rel="noopener noreferrer">Backloggd</a>
-              )</span>
+          <div id="coming-soon">
+            <h2>Coming Soon</h2>
+            <section className="coming-soon-container">
+              {comingSoon && <ComingSoon games={comingSoon.results.sort((a, b) => a.first_release_date - b.first_release_date)} list={5} />}
+            </section>
           </div>
         </section>
-      }
-      <section id="lp-body">
-        <div id="popular-this-month">
-          <h2>Popular this month - <Link to="/games">More</Link></h2>
-          <section className="games-container">
-            {popularThisMonth && <Games games={popularThisMonth.results} list={3} />}
-          </section>
-        </div>
-        <div id="coming-soon">
-          <h2>Coming Soon</h2>
-          <section className="coming-soon-container">
-            {comingSoon && <ComingSoon games={comingSoon.results.sort((a, b) => a.first_release_date - b.first_release_date)} list={5} />}
-          </section>
-        </div>
-      </section>
-      <section id="lp-reviews">
-        <h2>Recent Reviews - <span>More</span></h2>
-      </section>
-    </main>
+        <section id="lp-reviews">
+          <h2>Recent Reviews - <span>More</span></h2>
+        </section>
+      </main>
+    </>
   )
 }
 
